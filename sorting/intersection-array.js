73ee0a79c279bd967e320db1cb39978707a51eff
Kevin <46643116+ksbisht941@users.noreply.github.com>
2025-02-26 19:24:27 +0530

    Update intersection-array.js

--- sorting/intersection-array.js
+++ sorting/intersection-array.js
@@ -2,16 +2,22 @@ const arrA = [3, 5, 10, 10, 10, 15, 15, 20]
 const arrB = [5, 10, 10, 15, 30]
 
 function intersectionArray(arrA, arrB) {
-    while (arrA.length && arrB.length) {
-        if (arrA[0] === arrB[0]) {
-            return arrA[0];
-        }
-        if (arrA[0] < arrB[0]) {
-            arrA.shift();
+    let idx = 0, jdx = 0;
+    const result = [];
+    
+    while (idx < arrA.length && jdx < arrB.length) {
+        if (arrA[idx] == arrB[jdx]) {
+            result.push(arrA[idx]);
+            idx++;
+            jdx++;
+        } else if (arrA[idx] < arrB[jdx]) {
+            idx++;
         } else {
-            arrB.shift();
+            jdx++;
         }
     }
+    
+    return result;
 }
 
 const result = intersectionArray(arrA, arrB);
