4cd4ac052032966ca0e98970307e930b7578163e
ksbisht <ksbisht941@gmail.com>
2025-04-05 00:02:37 +0530

    longestSubarray

--- hashing/longest-subarray.js
+++ hashing/longest-subarray.js
@@ -1,21 +1,40 @@
-function longestSubarray(arr, sum) {
-    let res = 0;
-    for (let idx = 0; idx < arr.length; idx++) {
-        
-        let curr_sum = 0;
-        for (let jdx = idx; jdx < arr.length; jdx++) {
-            curr_sum += arr[jdx];
-            
-            if (curr_sum == sum) {
-                res = Math.max(res, jdx - idx + 1);
-            }
-        }
-        
-    }
+// function longestSubarray(arr, sum) {
+//     let res = 0;
+//     for (let idx = 0; idx < arr.length; idx++) {
 
-    return res;
+//         let curr_sum = 0;
+//         for (let jdx = idx; jdx < arr.length; jdx++) {
+//             curr_sum += arr[jdx];
+
+//             if (curr_sum == sum) {
+//                 res = Math.max(res, jdx - idx + 1);
+//             }
+//         }
+
+//     }
+
+//     return res;
+// }
+
+function longestSubarray(arr, sum) {
+  const hash = new Map();
+  let pre_sum = 0;
+  let res = 0;
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    pre_sum += arr[idx];
+
+    if (pre_sum === sum) res = idx + 1;
+    if (!hash.has(pre_sum)) hash.set(pre_sum, idx);
+
+    if (hash.has(pre_sum - sum)) {
+      res = Math.max(res, idx - hash.get(pre_sum - sum));
+    }
+  }
+
+  return res;
 }
 
 const arr = [5, 2, 3, 4];
-const result = longestSubarray(arr, 7)
+const result = longestSubarray(arr, 7);
 console.log(result);
